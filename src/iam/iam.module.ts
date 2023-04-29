66a08b4fdf8235382b2b1341fd20ab151d8a4cda
Adrian333Dev <adrian333dev@gmail.com>
2023-04-29 21:22:51 +0400

    Reset

--- src/iam/iam.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthenticationModule } from './authentication/authentication.module';
-
-@Module({
-  imports: [AuthenticationModule],
-})
-export class IamModule {}
